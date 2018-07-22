9a67c4307efe3bc3b17552d776e0ed93f36f15fd
efraim lorenzana <efraim.lorenzana@gmail.com>
2018-07-22 19:50:35 +0800

    clean the code for optimization

---
+++ src/container/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './App.css';
+// import { robots } from './components/js/data.js';
+import CardList from '../components/js/cardlist';
+import SearchBox from '../components/js/search';
+import Footer from '../components/js/footer';
+import '../components/js/clock';
+import Scroll from '../components/js/scroll';
+
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			robots: [],
+			searchTerm: ''
+		}
+	}
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then(response => response.json())
+			.then(user => this.setState({robots: user}));
+			//this.setState({robots: user})
+	}
+	searchOnChange = (e) => {
+		this.setState({ searchTerm: e.target.value})
+	}
+	render() {
+		const {robots, searchTerm} = this.state;
+		const filterRobot = robots.filter(r => {
+			return r.name.toLowerCase().includes(searchTerm.toLowerCase());
+		})
+
+		return !robots.length ? <h1 className="loading">Loading ...</h1> : (
+			<main id="mainApp" className="tc">
+				<section id="main">
+					<div id="header"><h1>Robot Friends</h1></div>
+					<SearchBox eventChange={this.searchOnChange} />
+					<Scroll>
+						<CardList robots={ filterRobot } />
+					</Scroll>
+				</section>
+				<Footer />
+			</main>
+		);
+		
+	}
+}
+
+export default App;
